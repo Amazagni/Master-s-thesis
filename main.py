5cec8150bd5d1aa41e6c0ac549a607cb8aa9b4d6
Amazagni <marcinxkomputer@gmail.com>
2026-03-06 09:56:13 +0100

    rotations added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from noise.salt_pepper import add_salt_and_pepper_noise
 from noise.gaussian import add_gaussian_noise
 from noise.blur import add_blur
 from noise.morfology import *
+from noise.rotation import *
 
 gen = ShapeGenerator(seed=42)
 
@@ -15,11 +16,16 @@ shape_generators = {
 }
 
 noise_functions = {
-    "salt_pepper": lambda img: add_salt_and_pepper_noise(img, 0.20, 42),
-    "gaussian": lambda img: add_gaussian_noise(img, 0.30, 42),
+    "salt_pepper": lambda img: add_salt_and_pepper_noise(img, 0.20, 42), # TODO stopnie
+    "gaussian": lambda img: add_gaussian_noise(img, 0.30, 42), #TODO sigma
     "blur": lambda img: add_blur(img),
     "erode": lambda img: erode(img),
     "dilate": lambda img: dilate(img),
+
+    "rotate_15": lambda img: rotate_image(img, 15), # TODO rozne wartosci (i losowe random_rotation)
+    "rotate_30": lambda img: rotate_image(img, 30),
+    "rotate_45": lambda img: rotate_image(img, 45),
+    "rotate_90": lambda img: rotate_image(img, 90),
 }
 
 for shape_name, shape_func in shape_generators.items():
